28c3315140c6b87b6377ac61b20fa4d889ef1a56
Aamir Alam <aamiralam1991@gmail.com>
2023-11-18 18:29:56 +0300

    fix scripts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const scheduledFunctions = require("./services/ScheduledFn");
+// const scheduledFunctions = require("./services/ScheduledFn");
 
 require("dotenv").config();
 
