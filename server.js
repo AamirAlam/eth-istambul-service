99a7fee1ba03abae88fa6c00a6d1f724659ac841
Aamir Alam <aamiralam1991@gmail.com>
2023-11-18 18:18:24 +0300

    fix buid

--- server.js
+++ server.js
@@ -1,5 +1,25 @@
-const app = require("./app");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const scheduledFunctions = require("./services/ScheduledFn");
+
+require("dotenv").config();
+
+//Init Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+// lol
+//Connect Database
+// scheduledFunctions.initScheduledJobs();
+
+const status = require("./routes/api/status");
+
+app.use("/api/v1/", status);
 
 const PORT = process.env.PORT || 5004;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
